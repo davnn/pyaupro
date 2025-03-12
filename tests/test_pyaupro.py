2855ffdbd5f425c654273525749508b26d49b884
David Muhr <muhrdavid+github@gmail.com>
2025-03-12 18:49:04 +0100

    :white_check_mark: Improve test data generation and parametrization

--- tests/test_pyaupro.py
+++ tests/test_pyaupro.py
@@ -3,12 +3,16 @@ import torch
 from pyaupro import PerRegionOverlap, auc_compute, generate_random_data
 from numpy.testing import assert_approx_equal
 
-preds, target = generate_random_data(seed=42)
-preds_, target_ = generate_random_data(seed=1337)
+noise_levels = [0.2, 0.4, 0.6]
+d0, d1, d2 = [generate_random_data(seed=i, noise_level=n) for i, n in enumerate(noise_levels)]
+test_data = [[d0], [d1, d2], [d0, d1, d2]]
 
-def test_exact_computation():
+@pytest.mark.parametrize("data", test_data)
+def test_exact_computation(data):
     metric = PerRegionOverlap(thresholds=None)
-    metric.update(preds, target)
+    for (preds, target) in data:
+        metric.update(preds, target)
+
     fpr, pro = metric.compute()
     assert isinstance(fpr, torch.Tensor)
     assert isinstance(pro, torch.Tensor)
@@ -16,12 +20,14 @@ def test_exact_computation():
     assert torch.all(fpr >= 0) and torch.all(fpr <= 1)
     assert torch.all(pro >= 0) and torch.all(pro <= 1)
 
-@pytest.mark.parametrize("thresholds", [5, [0.2, 0.4, 0.6, 0.8]])
-def test_approximated_computation(thresholds):
+@pytest.mark.parametrize("data", test_data)
+@pytest.mark.parametrize("thresholds", [5, 10, [0.2, 0.4, 0.6, 0.8]])
+def test_approximated_computation(data, thresholds):
     metric = PerRegionOverlap(thresholds=thresholds)
-    metric.update(preds, target)
-    fpr, pro = metric.compute()
+    for (preds, target) in data:
+        metric.update(preds, target)
 
+    fpr, pro = metric.compute()
     assert isinstance(fpr, torch.Tensor)
     assert isinstance(pro, torch.Tensor)
     n_thresh = len(thresholds) if isinstance(thresholds, list) else thresholds
@@ -29,30 +35,11 @@ def test_approximated_computation(thresholds):
     assert torch.all(fpr >= 0) and torch.all(fpr <= 1)
     assert torch.all(pro >= 0) and torch.all(pro <= 1)
 
-def test_multiple_updates_exact():
-    metric = PerRegionOverlap()
-    metric.update(preds, target)
-    metric.update(preds_, target_)
-    fpr, pro = metric.compute()
-
-    assert fpr.shape == pro.shape
-    assert torch.all(fpr >= 0) and torch.all(fpr <= 1)
-    assert torch.all(pro >= 0) and torch.all(pro <= 1)
-
-def test_multiple_updates_approximate():
-    metric = PerRegionOverlap(thresholds=10)
-    metric.update(preds, target)
-    metric.update(preds_, target_)
-    fpr, pro = metric.compute()
-
-    assert fpr.shape == pro.shape == (10,)
-    assert torch.all(fpr >= 0) and torch.all(fpr <= 1)
-    assert torch.all(pro >= 0) and torch.all(pro <= 1)
-
-@pytest.mark.parametrize("data", [[(preds, target)], [(preds, target), ((preds_, target_))]])
-def test_approx_auc_similar_to_exact_auc(data):
+@pytest.mark.parametrize("data", test_data)
+@pytest.mark.parametrize("thresholds", [100, torch.linspace(0, 1, 100)])
+def test_approx_auc_similar_to_exact_auc(data, thresholds):
     metric_exact = PerRegionOverlap(thresholds=None)
-    metric_approx = PerRegionOverlap(thresholds=1000)
+    metric_approx = PerRegionOverlap(thresholds=thresholds)
 
     for (preds, target) in data:
         metric_approx.update(preds, target)
@@ -63,8 +50,8 @@ def test_approx_auc_similar_to_exact_auc(data):
     fpr_exact, pro_exact = metric_exact.compute()
     auc_exact = auc_compute(fpr_exact, pro_exact, reorder=True)    
 
-    # with a large number of thresholds (1000), the values should be close
-    assert_approx_equal(auc_exact, auc_approx, significant=4)
+    # with a large enough number of thresholds, the values should be close
+    assert_approx_equal(auc_exact, auc_approx, significant=2)
 
 def test_invalid_inputs():
     with pytest.raises(ValueError):
